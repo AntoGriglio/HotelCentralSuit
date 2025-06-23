3cbfde486d76a8a44eda7cee9e08027e863ab81e
ANTO <antogriglio@gmail.com>
2025-06-23 17:26:11 -0300

    a

--- app/estadias/editar/page.tsx
+++ app/estadias/editar/page.tsx
@@ -3,7 +3,7 @@ import { Suspense } from 'react'
 
 export default function Page() {
   return (
-    <Suspense fallback={<p className="p-4">Cargando página...</p>}>
+    <Suspense fallback={<div className="p-4">Cargando...</div>}>
       <EditarEstadia />
     </Suspense>
   )
